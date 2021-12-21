e331e849efb4170d080085d8ce6d588aaae54e52
ArianTron <ariantron@yahoo.com>
2021-12-21 09:47:16 +0330

    + IEdmType.java #18

---
+++ src/main/java/io/githhub/TradersTeam/grand_node_java_client/model/IEdmType.java
@@ -0,0 +1,11 @@
+package io.githhub.TradersTeam.grand_node_java_client.model;
+
+import io.githhub.TradersTeam.grand_node_java_client.enums.EdmTypeKind;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class IEdmType {
+    private EdmTypeKind TypeKind;
+}
