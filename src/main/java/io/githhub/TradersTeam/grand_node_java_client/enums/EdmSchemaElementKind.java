f3d82cf193138d687188043eee19e450eede8a53
ArianTron <ariantron@yahoo.com>
2021-12-21 09:19:51 +0330

    + EdmSchemaElementKind.java #14

---
+++ src/main/java/io/githhub/TradersTeam/grand_node_java_client/enums/EdmSchemaElementKind.java
@@ -0,0 +1,16 @@
+package io.githhub.TradersTeam.grand_node_java_client.enums;
+
+public enum EdmSchemaElementKind {
+    None(0),
+    TypeDefinition(1),
+    Term(2),
+    Action(3),
+    EntityContainer(4),
+    Function(5);
+
+    int value;
+
+    EdmSchemaElementKind(int value) {
+        this.value = value;
+    }
+}
