f6f76e1e00e59505d000a3327fc7036bd8a903da
MansourM <sm.mirbehbahani@gmail.com>
2021-12-23 12:32:28 +0330

    +Customer.jva model #31

---
+++ src/main/java/io/githhub/TradersTeam/grand_node_java_client/models/Customer.java
@@ -0,0 +1,39 @@
+package io.githhub.TradersTeam.grand_node_java_client.models;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class Customer {
+    private String CustomerGuid; //uuid
+    private String Username;
+    private String Email;
+    private String adminComment;
+    private boolean isTaxExempt;
+    private boolean freeShipping;
+    private String AffiliateId;
+    private String VendorId;
+    private String StoreId;
+    private boolean Active;
+    private boolean Deleted;
+    private String Gender;
+    private String FirstName;
+    private String LastName;
+    private String DateOfBirth; //datetime
+    private String Company;
+    private String StreetAddress;
+    private String StreetAddress2;
+    private String ZipPostalCode;
+    private String City;
+    private String CountryId;
+    private String StateProvinceId;
+    private String Phone;
+    private String Fax;
+    private String VatNumber;
+    private String VatNumberStatusId;
+    private List<String> Groups;
+    private List<Address> Addresses;
+}
