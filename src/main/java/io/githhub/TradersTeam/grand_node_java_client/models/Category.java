cbd9bbcd1e7d202875364e2a14c2cea273510916
YaMiN <yamin8000@yahoo.com>
2021-12-23 11:27:14 +0330

    + Category.java #6

---
+++ src/main/java/io/githhub/TradersTeam/grand_node_java_client/models/Category.java
@@ -0,0 +1,35 @@
+package io.githhub.TradersTeam.grand_node_java_client.models;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class Category {
+    private String Id;
+    private String Name;
+    private String Description;
+    private String BottomDescription;
+    private String CategoryLayoutId;
+    private String MetaKeywords;
+    private String MetaDescription;
+    private String MetaTitle;
+    private String SeName;
+    private String ParentCategoryId;
+    private String PictureId;
+    private int PageSize;
+    private boolean AllowCustomersToSelectPageSize;
+    private String PageSizeOptions;
+    private boolean ShowOnHomePage;
+    private boolean FeaturedProductsOnHomaPage;
+    private boolean IncludeInTopMenu;
+    private boolean Published;
+    private int DisplayOrder;
+    private String ExternalId;
+    private String Flag;
+    private String FlagStyle;
+    private String Icon;
+    private boolean HideOnCatalog;
+    private boolean ShowOnSearchBox;
+    private int SearchBoxDisplayOrder;
+}
