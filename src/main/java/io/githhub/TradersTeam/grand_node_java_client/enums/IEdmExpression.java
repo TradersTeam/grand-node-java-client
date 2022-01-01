0b46d46c10a9e732a0094ada1bdc0e0a09d0108e
YaMiN <yamin8000@yahoo.com>
2022-01-01 11:25:00 +0330

    #35 #31

---
+++ src/main/java/io/githhub/TradersTeam/grand_node_java_client/enums/IEdmExpression.java
@@ -0,0 +1,36 @@
+package io.githhub.TradersTeam.grand_node_java_client.enums;
+
+public enum IEdmExpression {
+    None(0),
+    BinaryConstant(1),
+    BooleanConstant(2),
+    DateTimeOffsetConstant(3),
+    DecimalConstant(4),
+    FloatingConstant(5),
+    GuidConstant(6),
+    IntegerConstant(7),
+    StringConstant(8),
+    DurationConstant(9),
+    Null(10),
+    Record(11),
+    Collection(12),
+    Path(13),
+    If(14),
+    Cast(15),
+    IsType(16),
+    FunctionApplication(17),
+    LabeledExpressionReference(18),
+    Labeled(19),
+    PropertyPath(20),
+    NavigationPropertyPath(21),
+    DateConstant(22),
+    TimeOfDayConstant(23),
+    EnumMember(24),
+    AnnotationPath(25);
+
+    final int value;
+
+    IEdmExpression(final int value) {
+        this.value = value;
+    }
+}
