7f5f4d7b5fba16b99c82dc5f58ae836b18152e41
YaMiN <yamin8000@yahoo.com>
2022-01-01 10:50:15 +0330

    #10 #31

---
+++ src/main/java/io/githhub/TradersTeam/grand_node_java_client/models/DeleteAddress.java
@@ -0,0 +1,12 @@
+package io.githhub.TradersTeam.grand_node_java_client.models;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Builder
+@Setter
+public class DeleteAddress {
+    private String Address;
+}
