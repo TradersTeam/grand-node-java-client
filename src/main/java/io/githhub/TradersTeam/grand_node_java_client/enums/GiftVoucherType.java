36e522abc801ac772586442c614ba56c419ab1fa
YaMiN <yamin8000@yahoo.com>
2022-01-01 10:25:33 +0330

    #27

--- src/main/java/io/githhub/TradersTeam/grand_node_java_client/enums/GiftVoucherType.java
+++ src/main/java/io/githhub/TradersTeam/grand_node_java_client/enums/GiftVoucherType.java
@@ -3,6 +3,7 @@ package io.githhub.TradersTeam.grand_node_java_client.enums;
 public enum GiftVoucherType {
     Virtual(0),
     Physical(1);
+
     final int value;
 
     GiftVoucherType(int value) {
